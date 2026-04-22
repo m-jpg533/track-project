ec2aa3c7c25195ccd83d9cfca8fd187fcb50565d
ming shi <115329011+m-jpg533@users.noreply.github.com>
2026-04-22 15:06:06 +0800

    Change server port from 5000 to 5002

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ def logs_data():
     return jsonify(locations)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5002, debug=True)
